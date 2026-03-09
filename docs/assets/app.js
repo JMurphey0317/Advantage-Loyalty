006f5471b478ec339775c278e50ec18460cb8357
Dark Fenrir Forge <161866188+JMurphey0317@users.noreply.github.com>
2026-03-09 08:22:01 -0500

    Update assets/app.js to load Lightning Out and boot LWC

--- docs/assets/app.js
+++ docs/assets/app.js
@@ -1,4 +1,7 @@
-<script type="text/javascript" async="" src="https://acimacredit--preflight.sandbox.my.salesforce.com/lightning/lightning.out.latest/index.iife.prod.js"></script>
+// Load Lightning Out from Salesforce
 
-<lightning-out-application app-id="1UsVF0000000Fwj0AE" components="c-advantage-loyalty-lwc"></lightning-out-application>
-<c-advantage-loyalty-lwc></c-advantage-loyalty-lwc>
+const appId = 'YourAppId'; // Update with actual app ID
+
+$Lightning.use(appId, function() {
+    $Lightning.createComponent('c:advantageLoyaltyLwc', {}, 'lwc-root');
+});
